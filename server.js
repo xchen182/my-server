ae52a5dff7f66a40780f18d4d5ef46edb2b4ec4b
chenxizi <xchen182@hawk.iit.edu>
2019-10-18 14:27:40 -0500

    p&s

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http')  // require built-in Node.js http package
+
+// Use hosting values if available, otherwise default 
+const hostname = process.env.hostname || '0.0.0.0' // allow remote access
+const port = process.env.PORT || 3002
+
+// define our server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('This is Module 4: 01 Create a Node server / service\n')
+  res.write('This script is given by Prof.Case\n')
+  res.end('I just deploy it on heroku.\n')
+})
+
+// start listening
+// use the server console to tell user where to find the server
+// use backticks for template literals with embedded expressions
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://localhost:${port}/`)
+})
